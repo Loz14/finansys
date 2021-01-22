67f662cd3037e09534c45903aa193b784a0e7ce2
Luiz André Carvalho <luizandre.ita@gmail.com>
2021-01-22 14:39:29 -0300

    add: base-resource-form-component

---
+++ src/app/shared/components/base-resource-form/base-resource-form.component.ts
@@ -0,0 +1,135 @@
+import { OnInit, AfterContentChecked, Injector } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { BaseResourceModel } from "../../models/base-resource.model";
+import { BaseResourceService } from "../../services/base-resource.service";
+
+import { switchMap } from "rxjs/operators";
+
+import toastr from "toastr";
+
+export abstract class BaseResourceFormComponent<T extends BaseResourceModel> implements OnInit, AfterContentChecked {
+
+  currentAction: string;
+  resourceForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessage: string[] = null;
+  submittingForm: boolean = false;
+  protected route: ActivatedRoute
+  protected router: Router
+  protected formBuilder: FormBuilder
+
+  constructor(
+    protected injector: Injector,
+    protected resourceService: BaseResourceService<T>,
+    public resource: T,
+    protected jsonDataToResourceFn: (jsonData) => T
+  ) {
+    this.route = this.injector.get(ActivatedRoute)
+    this.router = this.injector.get(Router)
+    this.formBuilder = this.injector.get(FormBuilder)
+  }
+
+  ngOnInit(): void {
+    this.setCurrentAction();
+    this.buildResourceForm();
+    this.loadResource();
+  }
+
+  ngAfterContentChecked(): void {
+    this.setPageTitle()
+  }
+
+  submitForm(): void {
+    this.submittingForm = true;
+
+    if (this.currentAction == 'new')
+      this.createResource()
+    else
+      this.updateResource()
+  }
+
+  //PRIVATE METHODS
+
+  protected setPageTitle() {
+    if (this.currentAction == 'new')
+      this.pageTitle = this.creationPageTitle()
+    else {
+      this.pageTitle = this.editionPageTitle()
+    }
+  }
+
+  editionPageTitle(): string {
+    return 'Edição'
+  }
+
+  protected creationPageTitle(): string {
+    return 'Novo'
+  }
+
+  protected setCurrentAction() {
+    if (this.route.snapshot.url[0].path == 'new')
+      this.currentAction = 'new'
+    else
+      this.currentAction = 'edit'
+
+  }
+
+  protected loadResource() {
+    if (this.currentAction == 'edit') {
+      this.route.paramMap.pipe(
+        switchMap(params => this.resourceService.getById(Number(params.get("id"))))
+      )
+        .subscribe(
+          (resource) => {
+            this.resource = resource;
+            this.resourceForm.patchValue(resource); // set values on form
+          },
+          (error) => alert('Ocorreu um error no servidor, tente mais tarde!')
+        )
+    }
+  }
+
+  protected createResource() {
+    const resource: T = this.jsonDataToResourceFn(this.resourceForm.value)
+
+    this.resourceService.create(resource)
+      .subscribe(
+        resource => this.actionsForSuccess(resource),
+        error => this.actionsForError(error)
+      )
+  }
+
+  protected updateResource() {
+    const resource: T = this.jsonDataToResourceFn(this.resourceForm.value)
+
+    this.resourceService.update(resource).subscribe(
+      resource => this.actionsForSuccess(resource),
+      error => this.actionsForError(error)
+    )
+  }
+
+  protected actionsForSuccess(resource: T): void {
+    toastr.success("Solicitação processada com sucesso!");
+
+    const baseComponentPath: string = this.route.snapshot.parent.url[0].path;
+
+    this.router.navigateByUrl(baseComponentPath, { skipLocationChange: true }).then(
+      () => this.router.navigate([baseComponentPath, resource.id, 'edit'])
+    )
+  }
+
+  protected actionsForError(error: any): void {
+    toastr.error("Ocorreu um erro ao processar a sua solicitação!");
+
+    this.submittingForm = false;
+
+    if (error.status === 422)
+      this.serverErrorMessage = JSON.parse(error._body).errors; //depende da api
+    else
+      this.serverErrorMessage = ["Falha na comunicação com o servidor. Por favor, tente mais tarde!"]
+  }
+
+  protected abstract buildResourceForm(): void;
+}
