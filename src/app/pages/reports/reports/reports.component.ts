9df8b802944372e37aecd740eff6d21592ce53f3
Luiz André Carvalho <luizandre.ita@gmail.com>
2021-01-24 22:19:09 -0300

    remove log

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -58,7 +58,6 @@ export class ReportsComponent implements OnInit {
 
 
   private setValues(entries: Entry[]){
-    console.log("🚀 ~ ReportsComponent ~ setValues ~ entries", entries)
     this.entries = entries;
     this.calculateBalance();
     this.setChartData();
