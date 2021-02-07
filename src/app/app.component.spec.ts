d938b289eac4c2ec649561084c60b544599b344e
Luiz André Carvalho <luizandre.ita@gmail.com>
2021-02-07 19:01:50 -0300

    add first test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,34 @@
-it("can run a test", () => {
-    expect(1).toEqual(1);
-});
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { waitForAsync, ComponentFixture } from '@angular/core/testing'
+
+import { configureTests, ConfigureFn } from '../../test-config.helper'
+
+import { AppComponent } from './app.component';
+
+describe('Component snapshots', () => {
+
+    let fixture: ComponentFixture<AppComponent>;
+    let component: AppComponent;
+
+    beforeEach(
+        waitForAsync(() => {
+            const configure: ConfigureFn = testBed => {
+                testBed.configureTestingModule({
+                    declarations: [AppComponent],
+                    schemas: [NO_ERRORS_SCHEMA],
+                });
+            };
+
+            configureTests(configure).then(testBed => {
+                fixture = testBed.createComponent(AppComponent);
+                component = fixture.componentInstance;
+                fixture.detectChanges();
+            });
+        })
+    );
+
+    it("can run a test", () => {
+        expect(1).toEqual(1);
+    });
+
+})
